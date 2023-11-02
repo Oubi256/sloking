d305a12f565ba05878348c226805441571de9452
Oubi256 <98897017+Oubi256@users.noreply.github.com>
2023-11-03 00:46:03 +0100

    Added fake progressbar on splash

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,27 +1,56 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sloking/bloc/game_progress_bloc/game_progress_bloc.dart';
+import 'package:sloking/constants.dart';
+import 'package:sloking/widgets/general/fake_progress_bar.dart';
 import 'package:sloking/widgets/general/page_wrapper.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return BlocListener<GameProgressBloc, GameProgressState>(
-      listener: (context, state) {
-        Future.delayed(Duration(seconds: 2), () => context.go("/home"));
-        //context.go("/home");
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with SingleTickerProviderStateMixin {
+  final Duration randomSplashScreenDelay = Duration(seconds: Random().nextInt(3) + 4);
+  late final AnimationController _animationController = AnimationController(vsync: this, duration: Duration(seconds: randomSplashScreenDelay.inSeconds));
+  late final Animation<double> _progressBarController = CurveTween(curve: Curves.easeInOut).animate(_animationController);
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(milliseconds: 800),
+      () {
+        _animationController.forward().whenComplete(
+              () => Future.delayed(
+                Duration(milliseconds: 500),
+                () => context.go("/home"),
+              ),
+            );
       },
-      child: PageWrapper(
-        children: [
-          Positioned(width: 296.w, top: 78.h, bottom: 659.h, child: Image.asset("assets/images/splash/app_name.png")),
-          Positioned(top: 173.h, width: 628.w, bottom: -80.h, child: Image.asset("assets/images/splash/king.png")),
-        ],
-      ),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageWrapper(
+      children: [
+        Positioned(width: 296.w, top: 78.h, bottom: 659.h, child: Image.asset("assets/images/splash/app_name.png")),
+        Positioned(top: 173.h, width: 628.w, bottom: -20.h, child: Image.asset("assets/images/splash/king.png")),
+        Positioned(
+          bottom: 40.h,
+          left: 40.r,
+          right: 40.r,
+          height: 24.h,
+          child: FakeProgressBar(animation: _progressBarController),
+        ),
+      ],
     );
   }
 }
